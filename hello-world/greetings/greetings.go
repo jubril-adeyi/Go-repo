a63eda89bbdaddd54c9c07f28ca4b83056d50e77
jubril <root@DESKTOP-SH7JDD7.localdomain>
2023-08-16 22:27:43 +0100

    added error handler to greetings.go

--- hello-world/greetings/greetings.go
+++ hello-world/greetings/greetings.go
@@ -1,12 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// if no name is given, return an error with message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// Return a greeting that embeds the name in a message.
 	// message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	var message string
 	message = fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
