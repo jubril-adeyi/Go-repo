aac4525433c88856d5e4e2d1eead7d576ba6e151
Jubril Adeyi <jadeyi@gmail.com>
2023-08-16 21:00:56 +0100

    2

---
+++ Golang/greetings/greetings.go
@@ -0,0 +1,18 @@
+package greetings
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	// message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	var message string
+	message = fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
+
+//
+
+// def Hello(name):
+// 	message="Hi" + "welocme" + name
+// 	return message
